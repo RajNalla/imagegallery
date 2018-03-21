62a196d7d95dae8ba2d158faed9ece3261c2d4af
nikhithraj <replyme.nikhithraj@gmail.com>
2018-03-21 12:25:53 -0400

    GitHubApp @Angular2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
-
+import { GithubService} from './services/github.services';
 @Component({
+  
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">
+       GIT-HUB-APP
+      </a>
+    </div>
+  </div>
+</nav>
+            <div class="container"><profile></profile></div>`,
+  providers:[GithubService]
+  
 })
 export class AppComponent {
   title = 'app';
